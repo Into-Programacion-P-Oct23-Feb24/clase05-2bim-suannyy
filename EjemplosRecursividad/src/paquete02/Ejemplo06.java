7e9604c56258b8e15c3d7594bec05be13d9ee1c1
suannyy <sjmayon1@utpl.edu.ec>
2024-01-11 17:33:37 -0500

    comentarios

--- EjemplosRecursividad/src/paquete02/Ejemplo06.java
+++ EjemplosRecursividad/src/paquete02/Ejemplo06.java
@@ -20,21 +20,27 @@ public class Ejemplo06 {
             System.out.printf("El valor es: %d\n", 
                     obtenerSuma(valor1[i], valor2[i]));
         }
-        
+        // for hace que se ejecutará mientras la condición especificada sea verdadera.
+        //En este caso, la variable i se inicializa en 0
         for (int i = 0; i < valor1.length; i++) {
             int num1 = valor1[i];
             int num2 = valor2[i];
             System.out.printf("El valor es: %d\n", 
                     obtenerSuma(num1, num2));
         }
-        
+        //realiza la misma acción que el código anterior, pero en lugar de pasar
+        //directamente valor1[i] y valor2[i] a la función obtenerSuma, los 
+        //elementos se almacenan primero en las variables num1 y num2, y luego
+        //se utilizan en la llamada a la función.
         for (int i = 0; i < valor1.length; i++) {
             int num1 = valor1[i];
             int num2 = valor2[i];
             int suma = obtenerSuma(num1, num2); 
             System.out.printf("El valor es: %d\n", suma);
         }
-        
+        // este código realiza la misma operación que los ejemplos anteriores, 
+        //pero separa explícitamente la asignación y almacenamiento del 
+        //resultado de la suma en una variable suma antes de imprimirlo en pantalla.
         
 
     }
