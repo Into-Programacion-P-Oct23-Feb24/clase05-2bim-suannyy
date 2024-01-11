a75288ab0ddea8353242ebc8a2aed43789ec4a55
suannyy <sjmayon1@utpl.edu.ec>
2024-01-11 15:56:24 -0500

    clase01

---
+++ EjemplosRecursividad/src/Paquete03/NewClass.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Paquete03;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class NewClass {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static String[][] obtenerCadena() {
+        String[][] cadena = {{"LOJA", "CUENCA", "QUITO"},
+        {"GUAYAQUIL", "ZAMORA", "MACHALA"}};
+        return cadena;
+
+    }
+}
